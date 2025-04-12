2bac4daf6d54bc2ad20cd19b8d48a02090ddd413
minhhoang2904 <buileminhhoang2005@gmail.com>
2025-04-11 22:05:29 -0400

    version 1

--- 1/problem.py
+++ 1/problem.py
@@ -1,7 +1,22 @@
 class Solution:
     def planVacation(self, miles, target):
-        # Time complexity: 
-        # Space complexity:
+        # Time complexity: O(n)
+        # Space complexity: O(1)
         
         # Remove pass and write code here
-        pass
+        left = 0
+        min_days = len(miles)
+        sum = 0
+        for i in range(len(miles)):
+            sum += miles[i]
+
+            while(sum >= target):
+                sum -= miles[left]
+                min_days = min(min_days, i - left +1)
+                left += 1
+
+        return min_days 
+
+
+
+        
--- 2/problem.py
+++ 2/problem.py
@@ -1,7 +1,35 @@
 class Solution:
     def isValidSudoku(self, matrix):
-         # Time complexity: 
-        # Space complexity:
+         # Time complexity: O(n)
+        # Space complexity: 
         
         # Remove pass and write code here
-        pass
+        for i in range(9):
+            row = []
+            for j in range(9):
+                if matrix[i][j]!= ".":
+                    row.append(matrix[i][j])
+            if (len(row) != len(set(row))):
+                return False
+            
+        for i in range(9):
+            col = []
+            for j in range(9):
+                if matrix[i][j]!= ".":
+                    col.append(matrix[i][j])
+            if (len(row) != len(set(row))):
+                return False
+        
+        for box_r in range(0, 9, 3):
+            for box_c in range(0, 9, 3):
+                block = []
+                for i in range(box_r, box_r + 3):
+                    for j in range(box_c, box_c + 3):
+                        if matrix[i][j] != '.':
+                            block.append(matrix[i][j])
+                if len(block) != len(set(block)):
+                    return False
+                
+        return True
+    
+
